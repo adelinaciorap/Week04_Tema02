fe9dee8a0588e7876a3aba1ad83d3ced6d0e2c8b
Adelina Ciorap <c.adelina@outlook.com>
2019-05-13 21:20:54 +0300

    1

---
+++ 1.Queue/1.Queue/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            
+            Queue inline = new Queue();
+            inline.Enqueue("Maria");
+            inline.Enqueue("Ana");
+            inline.Enqueue("Dan");
+            inline.Enqueue("Gigi");
+
+            inline.Dequeue();
+
+            inline.Print();
+            
+        }
+        
+    }
+   
+
+}
---
+++ 1.Queue/1.Queue/Queue.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1.Queue
+{
+    class Queue
+    {
+        private List <string> myqueue = new List <string>();
+
+        public void Enqueue(string order)
+        {
+            myqueue.Add(order);
+        }
+
+        public void Dequeue()
+        {
+            if (myqueue.Count >0)
+            { 
+                myqueue.RemoveAt(0);
+            }
+            else
+                Console.WriteLine("The queue is empty");
+
+        }
+        public void Print()
+        {
+            foreach (string a in myqueue)
+            {
+                Console.WriteLine(a + ",");
+            }
+        }
+        
+
+    }
+
+    
+}
