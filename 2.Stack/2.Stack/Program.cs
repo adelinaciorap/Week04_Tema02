6fda61c2599d205bf09747474715f5fba218e83e
Adelina Ciorap <c.adelina@outlook.com>
2019-05-13 21:30:18 +0300

    1

---
+++ 2.Stack/2.Stack/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Stack test = new Stack();
+            
+            test.Push(1);
+            test.Push(2);
+            test.Push(3);
+            test.Push(4);
+            test.Push(5);
+            
+            test.Print();
+            
+            test.Pop();
+            test.Pop();
+            
+            Console.WriteLine();
+            test.Print();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 2.Stack/2.Stack/Stack.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _2.Stack
+{
+    class Stack
+    {
+        private List<int> ontop = new List<int>();
+
+        public void Push(int num)
+        {
+            ontop.Add(num);
+        }
+
+        public void Pop()
+        {
+            if (ontop.Count > 0)
+                ontop.Remove(ontop.Count);
+            else
+                Console.WriteLine("The stack is empty.");
+        }
+
+        public void Print()
+        {
+            foreach (int b in ontop)
+                Console.Write(b + " ");
+        }
+    }
+}
